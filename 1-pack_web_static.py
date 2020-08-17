114763a7529d657131ec28cb52c60965a40f3077
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-17 14:08:15 -0500

    cript that generates a .tgz archive from the contents of the web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+"""generates a .tgz archive"""
 from fabric.api import local
 from datetime import datetime
 
 
 
 def do_pack():
+    """contents of the web_static"""
     f = datetime.now()
 
     file_name = "web_static_{}{}{}{}{}{}.tgz".format(
         f.year, f.month, f.day, f.hour, f.minute, f.second)
 
     local('mkdir -p versions')
-    local('tar -cvf versions/{} web_static'.format(file_name))
+    result = local('tar -cvf versions/{} web_static'.format(file_name))
     print("Packing web_static to versions/{}".format(file_name))
 
-    return "versions/{}".format(file_name)
+    if result == 0:
+        return "versions/{}".format(file_name)
+    return None
