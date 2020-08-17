922c8d51cfdb857d8620c03ba50b85e81a514095
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-17 15:09:58 -0500

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers,

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,8 +3,6 @@
 from fabric.api import local, env, put, run
 from datetime import datetime
 from os import path
-
-
 env.hosts = ['35.196.97.61', '34.75.200.40']
 env.user = 'ubuntu'
 
@@ -16,21 +14,18 @@ def do_deploy(archive_path):
         return False
 
     archive_file = archive_path.split('/')[1]
-    name_file = archive_file.split('.tgz')
-    release = '/data/web_static/releases/{}'.format(name_file)
+    name_file = archive_file.split('.')[0]
+    release = '/data/web_static/releases/{}/'.format(name_file)
 
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, '/tmp')
         run('mkdir -p {}'.format(release))
         run('tar -xzf /tmp/{} -C {}'.format(archive_file, release))
         run('rm -rf /tmp/{}'.format(archive_file))
         run('mv {}//web_static/* {}'.format(release, release))
         run('rm -rf {}/web_static'.format(release))
-        
-        current_file = '/data/web_static/current'
-        run('rm -rf {}'.format(current_file))
-        run('ln -sf {} {}'.format(current_file, release))
+        run('rm -rf /data/web_static/current'.format(current_file))
+        run('ln -s {} /data/web_static/current'.format(current_file, release))
         return True
     except Exception as e:
         return False
-
