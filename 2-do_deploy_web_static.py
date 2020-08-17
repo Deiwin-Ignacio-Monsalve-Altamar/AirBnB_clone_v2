77222383c26469d7e7819c486b48478a85657dda
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-17 15:13:17 -0500

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers,

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 """that distributes an archive"""
-from fabric.api import local, env, put, run
+from fabric.api import local, put, env, run
 from datetime import datetime
-from os import path
-env.hosts = ['35.196.97.61', '34.75.200.40']
+from os import path, stat
+
+env.hosts = ['35.237.62.36', '104.196.53.129']
 env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """Script distributes an archive"""
-
     if not path.isfile(archive_path):
         return False
 
     archive_file = archive_path.split('/')[1]
-    name_file = archive_file.split('.')[0]
-    release = '/data/web_static/releases/{}/'.format(name_file)
+    file_no_ext = archive_file.split('.')[0]
+    releases = '/data/web_static/releases/{}/'.format(file_no_ext)
 
     try:
         put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(release))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_file, release))
-        run('rm -rf /tmp/{}'.format(archive_file))
-        run('mv {}//web_static/* {}'.format(release, release))
-        run('rm -rf {}/web_static'.format(release))
-        run('rm -rf /data/web_static/current'.format(current_file))
-        run('ln -s {} /data/web_static/current'.format(current_file, release))
+        run('mkdir -p {}'.format(releases))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_file, releases))
+        run('rm /tmp/{}'.format(archive_file))
+        run('mv {}/web_static/* {}'.format(releases, releases))
+        run('rm -rf {}/web_static'.format(releases))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases))
         return True
     except Exception as e:
         return False
