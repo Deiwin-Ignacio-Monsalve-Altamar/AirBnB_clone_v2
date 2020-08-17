227cb0146c30a191585f0a2f8ee70b70c1fbc814
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-17 13:59:09 -0500

    cript that generates a .tgz archive from the contents of the web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,14 +4,15 @@ from datetime import datetime
 
 env.hosts['localhost']
 
+
 def do_pack():
     f = datetime.now()
-    
-    file_name = "web_static_{}{}{}{}{}{}.tgz".format(f.year, f.month, f.day, f.hour, f.minute, f.second)
-    
+
+    file_name = "web_static_{}{}{}{}{}{}.tgz".format(
+        f.year, f.month, f.day, f.hour, f.minute, f.second)
+
     local('mkdir -p versions')
     local('tar -cvf versions/{} web_static'.format(file_name))
     print("Packing web_static to versions/{}".format(file_name))
-    
+
     return "versions/{}".format(file_name)
-    
