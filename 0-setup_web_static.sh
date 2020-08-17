13538c648d6061cc37eb8abc38ab40b2d0b3be4f
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-17 13:21:02 -0500

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,11 +12,10 @@ sudo mkdir -p "/data/web_static/releases/test/"
 sudo touch "/data/web_static/releases/test/index.html"
 sudo echo "some simple data" | sudo tee "/data/web_static/releases/test/index.html"
 
-target="/data/web_static/current"
-link_name="/data/web_static/releases/test/"
-sudo ln -sf "$link_name" "$target"
-data="/data/"
-sudo chown -R ubuntu:ubuntu "$data"
+link_file="/data/web_static/current"
+destination="/data/web_static/releases/test/"
+sudo ln -sf "$destination" "$link_file"
+sudo chown -R ubuntu:ubuntu /data/
 config="/etc/nginx/sites-enabled/default"
-sudo sed -i "38i \\\\tlocation /hbnb_static/ {\n\t\talias $target/;\n\t\tautoindex off;\n\t}\n" "$config"
+sudo sed -i "38i \\\\tlocation /hbnb_static/ {\n\t\talias $link_file/;\n\t\tautoindex off;\n\t}\n" "$config"
 sudo service nginx restart
