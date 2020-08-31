0061b2c4167b2f8bd9bbd62575fb8fd51fb5dc4e
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-31 10:40:07 -0500

    Write a script that starts a Flask web application:

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+"""starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """starts a Flask web application"""
+    return 'Hello, HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
