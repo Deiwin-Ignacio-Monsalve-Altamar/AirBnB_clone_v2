7476f844c8b7d6ac6d3fc043c9838982978d06b1
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-31 11:29:04 -0500

    script that starts a Flask web application:

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/bin/usr/python
+"""/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text variable
+(replace underscore _ symbols with a space )
+/python/(<text>): display “Python ”, followed by the value of the
+text variable (replace underscore _ symbols with a space )
+The default value of text is “is cool
+/number/<n>: display “n is a number” only if n is an integer"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """starts a Flask web hello_world"""
+    return 'Hello, HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnh():
+    """starts a Flask web page /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_fun(text='is cool'):
+    """ followed by the value of the text python"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number_is_exists(n):
+    """display “n is a number” only if n is an integer"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
