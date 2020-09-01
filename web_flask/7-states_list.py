0e5e3749280c893982e8a95be71928ff7cc9fb16
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-01 12:38:23 -0500

    fix function on errors in tasks 4 56

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,6 +9,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown(self):
+    """reload close files storage"""
     storage.close()
 
 
